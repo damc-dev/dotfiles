a1bc94d1455a017f04f7d82429714fb3490e2260
David McElligott <damc.dev@gmail.com>
2016-11-30 09:04:05 -0600

    Removed time elapsed from function

--- bash/bashrc.d/recentRpms.bash
+++ bash/bashrc.d/recentRpms.bash
@@ -1,10 +1,8 @@
 recentRpms() {
-  SECONDS=0
   NUM=5
   if [ "$1" != "" ]; then
     NUM="$1"
   fi
   rpm -qa  --queryformat '%{installtime} (%{installtime:date}) %{name}\n' | sort -n -r | head -$NUM
-  echo $SECONDS
 }
 
