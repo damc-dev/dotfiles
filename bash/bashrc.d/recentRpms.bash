32314ee2cf329d91fdf8b3d8f48567b344aebfee
David McElligott <damc.dev@gmail.com>
2016-11-30 09:01:56 -0600

    Added command to list recently installed RPMs

---
+++ bash/bashrc.d/recentRpms.bash
@@ -0,0 +1,10 @@
+recentRpms() {
+  SECONDS=0
+  NUM=5
+  if [ "$1" != "" ]; then
+    NUM="$1"
+  fi
+  rpm -qa  --queryformat '%{installtime} (%{installtime:date}) %{name}\n' | sort -n -r | head -$NUM
+  echo $SECONDS
+}
+
