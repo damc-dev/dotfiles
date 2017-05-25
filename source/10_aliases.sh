0605ad9f8fa2a39280ba5c9c218b4373ce5e8c56
David McElligott <EMAIL>
2017-05-25 08:14:51 -0500

    modified bashrc to only source *.sh files and added aliases

--- source/10_aliases.sh
+++ source/10_aliases.sh
@@ -3,12 +3,36 @@ if [ -f ~/.aliases ]; then
   source ~/.aliases
 fi
 
-# Replace newlines with commas
-listjoin() {
-  cat $@ | awk -vORS=, 'NF { print $1 }'| sed 's/,$/\n/'
-}
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
 
-# File get
-fget () {
-  curl -O "$@"
-}
+# Always enable colored `grep` output
+# Note: `GREP_OPTIONS="--color=auto"` is deprecated, hence the alias usage.
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Stopwatch
+alias timer='echo "Timer started. Stop with Ctrl-D." && date && time cat && date'
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
+
+# List Largest Files
+alias largest_files='du -ah | sort -hr | head -n 10'
+
+# List Largest Directories
+alias largest_directories='du -sh * | sort -hr | head -n 10'
+
+# Intuitive map function
+# For example, to list all directories that contain a certain file:
+# find . -name .gitattributes | map dirname
+alias map="xargs -n1"
+
+# URL-encode strings
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+
+alias reload='source ~/.bashrc'
